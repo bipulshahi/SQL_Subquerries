27194f3861054fc1ba547b97330bbd17f39999f9
Bipul Shahi <bipulshahi25@gmail.com>
2025-10-01 10:11:26 +0530

    Create correlated_2.sql

---
+++ correlated_2.sql
@@ -0,0 +1,93 @@
+-- List customers who have placed orders with at least two different statuses (e.g., "Completed", "Pending").
+SELECT c.customer_id, c.name,
+       COUNT(DISTINCT o.status) AS num_statuses
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name;
+
+select a.name from
+(SELECT c.customer_id, c.name,
+       COUNT(DISTINCT o.status) AS num_statuses
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name) a
+where a.num_statuses > 1;
+
+-- correlated
+SELECT c.name
+FROM customers c
+WHERE (
+    SELECT COUNT(DISTINCT o.status)
+    FROM orders o
+    WHERE o.customer_id = c.customer_id
+) > 1;
+
+-- non-correlated
+SELECT c.customer_id, c.name
+FROM customers c
+JOIN (
+    SELECT customer_id
+    FROM orders
+    GROUP BY customer_id
+    HAVING COUNT(DISTINCT status) >= 2
+) t ON c.customer_id = t.customer_id;
+
+
+-- Retrieve the orders that have  amount higher than the average amount of all orders placed by the same customer.
+-- correlated
+SELECT o.order_id, o.customer_id, o.total, c.name
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+WHERE o.total > (
+    SELECT AVG(o2.total)
+    FROM orders o2
+    WHERE o2.customer_id = o.customer_id);
+    
+-- non-correlated 
+SELECT o.order_id, o.customer_id, o.total, c.name
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN (
+    SELECT customer_id, AVG(total) AS avg_total
+    FROM orders
+    GROUP BY customer_id
+) t ON o.customer_id = t.customer_id
+WHERE o.total > t.avg_total;
+
+-- Find products that have been ordered by customers from multiple cities.
+select * from orders;
+select * from order_items;
+select * from products;
+select * from customers;
+-- correlated
+SELECT p.name FROM products p
+WHERE (
+    SELECT COUNT(DISTINCT c.city)
+    FROM customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+    JOIN order_items oi ON o.order_id = oi.order_id
+    WHERE oi.product_id = p.product_id
+) > 1;
+
+-- non correlated
+SELECT p.name
+FROM products p
+JOIN (
+    SELECT oi.product_id, COUNT(DISTINCT c.city) AS city_count
+    FROM order_items oi
+    JOIN orders o ON oi.order_id = o.order_id
+    JOIN customers c ON o.customer_id = c.customer_id
+    GROUP BY oi.product_id
+    HAVING COUNT(DISTINCT c.city) > 1
+) t ON p.product_id = t.product_id;
+
+
+SELECT o.customer_id, c.name, o.order_id, 
+       o.total - (SELECT AVG(o2.total) 
+                  FROM orders o2 
+                  WHERE o2.customer_id = o.customer_id) AS margin
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id;
+
+
+
