80991cfa18343e9f8c314af9385113e69052bb4e
Bipul Shahi <bipulshahi25@gmail.com>
2025-10-06 11:10:35 +0530

    Update correlated_2.sql

--- correlated_2.sql
+++ correlated_2.sql
@@ -173,6 +173,20 @@ SELECT o.customer_id, c.name, o.order_id,
 FROM orders o
 JOIN customers c ON o.customer_id = c.customer_id;
 
+
+SELECT 
+    o.customer_id,
+    c.name,
+    o.order_id,
+    o.total - t.avg_total AS margin
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN (
+    SELECT customer_id, AVG(total) AS avg_total
+    FROM orders
+    GROUP BY customer_id
+) t ON o.customer_id = t.customer_id;
+
 /*
 Modify the query to classify each order using a CASE expression:
 'Above Average' if margin > 0
